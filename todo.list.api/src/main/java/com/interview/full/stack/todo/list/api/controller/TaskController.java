9204a7c37bceaf9297b90a721e6a3ead1dd30134
Merzouke Mansouri <merzouke@m14i.com>
2025-01-10 13:08:41 +0100

    Add cross-origin support for TaskController

--- todo.list.api/src/main/java/com/interview/full/stack/todo/list/api/controller/TaskController.java
+++ todo.list.api/src/main/java/com/interview/full/stack/todo/list/api/controller/TaskController.java
@@ -2,16 +2,23 @@ package com.interview.full.stack.todo.list.api.controller;
 
 
 import com.interview.full.stack.todo.list.api.dto.TaskDTO;
+import com.interview.full.stack.todo.list.api.payload.TaskPayload;
 import com.interview.full.stack.todo.list.api.service.TaskService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST })
 @RequiredArgsConstructor
 public class TaskController {
 
@@ -20,6 +27,11 @@ public class TaskController {
     @GetMapping("")
     public List<TaskDTO> getTasks() {
         return taskService.getTasks();
+    }
+
+    @PostMapping("")
+    public TaskDTO createTask(@RequestBody @Valid TaskPayload payload) {
+        return taskService.createTask(payload);
     }
 
 }
