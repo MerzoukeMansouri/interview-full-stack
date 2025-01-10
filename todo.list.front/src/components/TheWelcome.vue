a332ed274263a3911eec1c6aa74a874accde082c
Merzouke Mansouri <merzouke@m14i.com>
2025-01-10 12:50:47 +0100

    initial front commit

---
+++ todo.list.front/src/components/TheWelcome.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+
+const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+  <div class="welcome-container">
+    <WelcomeItem>
+      <template #icon>
+        <DocumentationIcon />
+      </template>
+      <template #heading>Info</template>
+
+      List, create, edit your tasks, and more.
+    </WelcomeItem>
+
+    <WelcomeItem>
+      <template #icon>
+        <ToolingIcon />
+      </template>
+      <template #heading>Is backend up ?</template>
+
+      Make sure http://localhost:8080/tasks is running.
+    </WelcomeItem>
+
+    <WelcomeItem>
+      <template #icon>
+        <EcosystemIcon />
+      </template>
+      <template #heading>Ecosystem</template>
+
+      vuejs3 and java spring boot
+    </WelcomeItem>
+  </div>
+</template>
+<style scoped>
+.welcome-container{
+  min-height: 500px;
+}
+</style>
---
+++ todo.list.front/src/views/ToDoView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    //TODO FORM LIST THEM
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    display: flex;
+    align-items: center;
+    min-height: 500px;
+
+  }
+}
+</style>
