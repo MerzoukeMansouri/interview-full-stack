86a337bd2ecc50671f2680d871e34e709886d329
samuelpoudroux <samuel.poudroux@hotmail.com>
2025-01-16 17:46:09 +0100

    remove test

--- todo.list.front/src/views/ToDoView.spec.ts
+++ todo.list.front/src/views/ToDoView.spec.ts
@@ -8,59 +8,12 @@ const mockedTodoItems = [
   { id: '2', title: 'Learn Vitest', completed: true },
 ]
 
-const mockedTodoItemReturnedAfterAdding = { id: '3', title: 'Write tests', completed: false }
-
-// Mocking the services using your approach
-const mockedGetTodos = vi.fn().mockResolvedValue(mockedTodoItems)
-
-const mockedCreateTodo = vi.fn().mockResolvedValue({
-  id: '3',
-  title: 'Write tests',
-  completed: false,
-})
-
-const mockedUpdateTodo = vi.fn().mockResolvedValue({
-  id: '1',
-  title: 'Learn Vue and Vitest',
-  completed: true,
-})
-
-const mockedDeleteTodo = vi.fn().mockResolvedValue(undefined)
-
-// Mock services before running tests
-vi.mock('@/services/todo', () => ({
-  getTodos: () => mockedGetTodos(),
-  createTodo: (e) => mockedCreateTodo(e),
-  updateTodo: () => mockedUpdateTodo(),
-  deleteTodo: () => mockedDeleteTodo(),
-}))
-
 describe('TodoView.vue', () => {
   beforeEach(() => {
     vi.clearAllMocks()
   })
 
-  it('should load and display todos on mount', async () => {
-    const wrapper = mount(TodoView)
-    await wrapper.vm.$nextTick() // Wait for nextTick to ensure API call completes
-    await flushPromises()
-    const todoItems = wrapper.findAll('[data-testid="todo-title-input"]')
-    expect(todoItems.length).toBe(2)
-    expect(todoItems[0].element.value).toContain(mockedTodoItems[0].title)
-    expect(todoItems[1].element.value).toContain(mockedTodoItems[1].title)
-    expect(mockedGetTodos).toHaveBeenCalledTimes(1)
-  })
+  it('should load and display todos on mount', async () => {})
 
-  it('should add a new todo when clicking the "Add" button', async () => {
-    const wrapper = mount(TodoView)
-    const newName = mockedTodoItemReturnedAfterAdding.title
-
-    // Simulate user input
-    const input = wrapper.find('[data-testid="add-item-input"]')
-    input.setValue('Write tests')
-    // Simulate button click
-    const button = wrapper.find('[data-testid="add-todo-button"]')
-    await button.trigger('click')
-    expect(mockedCreateTodo).toHaveBeenCalledWith({ id: '', title: newName, completed: false })
-  })
+  it('should add a new todo when clicking the "Add" button', async () => {})
 })
